bfbb4b11d043f1862b806008c663aba7228f551e
Yu Otsuka <yuotsu05@gmail.com>
2026-03-10 21:41:09 +0900

    chore: setup Capacitor and local-notifications plugin

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yuotsuka.shukatsuboard',
+  appName: 'ShukatsuBoard',
+  webDir: 'out'
+};
+
+export default config;
